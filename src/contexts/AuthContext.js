622bc70543fe3ffdd24ae854d3f652fc4b56cc77
OlirEswar <92946878+OlirEswar@users.noreply.github.com>
2022-08-06 19:02:41 -0400

    authentication with firebase

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,40 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useContext, useState, useEffect  } from "react";
+import { auth } from "../firebase"
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+
+    const [currentUser, setCurrentUser] = useState()
+
+    function signup(email, password) {
+       return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+        })
+
+        return unsubscribe
+    }, [])
+
+    auth.onAuthStateChanged(user => {
+        setCurrentUser(user)
+    })
+
+    const value = {
+        currentUser,
+        signup
+    }
+    return (
+        <AuthContext.Provider value={value}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
