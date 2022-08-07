bebef8d2191dd5f4c50fcb49a724201d7a9a4c1f
nepthius <anishdshekar@gmail.com>
2022-08-07 00:03:20 -0400

    Added both classify and generate functionality

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ const users = []
 //Username
 let username = ''
 
+//Stores whether it is generating or classifying
+test = 0
+
 //connect to CockroachDB
 server.listen(3000)
 
@@ -104,7 +107,13 @@ app.delete('/logout', (req, res) => {
 })
 
 //talk to chatbot
+app.get('/generate', (req,res) => {
+    test = 1
+    res.render('generate.ejs')
+})
+
 app.get('/classify', (req,res) => {
+    test = 2
     res.render('classify.ejs')
 })
 
@@ -126,40 +135,60 @@ function checkNotAuthenticated(req, res, next){
 
 //Stores prompt
 p = 'Question: How do I stay focused?\nAnswer: Eliminate distractions \n--\nQuestion: How do I eat less?\nAnswer: Drink more water.\n--\nQuestion: How can I stay calm?\nAnswer: Take deep breaths.\n--\nQuestion: How to become less hungry?\nAnswer: Don\'t eat too much.\n--\nQuestion: How to stay happy?\nAnswer: Eat more.\n--\n'
-//Stores whether it is generating or classifying
-test = 0
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       console.log(username)
       io.emit('chat message', (username + ': ' + msg));
-      p += `Question: ${msg}` + '\nAnswer: ';
-      (async () => {
-        const response = await cohere.generate({
-          model: 'small',
-          prompt: p,
-          max_tokens: 50,
-          temperature: 0.9,
-          k: 0,
-          p: 0.75,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-          stop_sequences: ["--"],
-          return_likelihoods: 'NONE'
-        });
-        s = `${response.body.generations[0].text}`
-        s = s.split("--")
-        for(let i = 0; i < s.length; i++){
-          if(s.includes('Question:') || s.includes('Answer:')){
-            break
-          }
-          if(s != '\n' || s != '' || s!=null){
-            io.emit('chat message', ("Chatbot: " + s[i]))
-          }
-          
-        }
-        
-      })();
+      console.log(test)
+
+      //Generates chatbot text to talk to user
+      if(test == 1){
+        p += `Question: ${msg}` + '\nAnswer: ';
+        console.log(p);
+        (async () => {
+            const response = await cohere.generate({
+                model: 'small',
+                prompt: p,
+                max_tokens: 50,
+                temperature: 0.9,
+                k: 0,
+                p: 0.75,
+                frequency_penalty: 0,
+                presence_penalty: 0,
+                stop_sequences: ["--"],
+                return_likelihoods: 'NONE'
+            });
+            s = `${response.body.generations[0].text}`
+            s = s.split("--")
+            for(let i = 0; i < s.length; i++){
+                console.log("String piece:" + s[i])
+                if(s[i].includes('Question:') || s[i].includes('Answer:') || s[i].includes('                           ')){
+                    console.log("it has a question or answer")
+                    break
+                }
+                else{
+                    if(s != '\n' || s != '' || s!=null){
+                        io.emit('chat message', ("Chatbot: " + s[i]))
+                        p += s[i]
+                    }
+            }
+            p+= '\n--\n'
+            
+            }
+            
+        })();
+      }
+
+      else if(test == 2){
+        (async () => {
+            const response = await cohere.classify({
+              model: '489cf173-a605-4ed6-9a90-67a97244673f-ft',
+              inputs: [msg]
+            });
+            io.emit('chat message', (`Chatbot: Your statement is perceived as ${((response.body.classifications)[0]).prediction}`));
+          })();
+      }
     });
 });
 
